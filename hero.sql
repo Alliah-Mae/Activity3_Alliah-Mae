a46b480baf36bc3897c9e8f98ab1caae8e6e7643
Alliah-Mae <alliahmaelaylo2@gmail.com>
2023-11-14 10:57:29 +0800

    feat select active players

--- hero.sql
+++ hero.sql
@@ -164,3 +164,9 @@ VALUES
 
     DELETE from public.hero item
     WHERE hero_id = 1
+
+    --5
+    SELECT p.player_name, h.hero_name
+    FROM public.player p
+    INNER JOIN public.hero h ON p.hero_id = h.hero_id
+    WHERE h.is_active = true;
